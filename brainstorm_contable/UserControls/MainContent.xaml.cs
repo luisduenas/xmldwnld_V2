a8dd5edc559eea8771536a6700c4574815b8fdaf
luisduenas <luis.alonso.duenas.m@gmail.com>
2020-04-12 18:58:17 -0700

    first commit

---
+++ brainstorm_contable/UserControls/MainContent.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace brainstorm_contable.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MainContent.xaml
+    /// </summary>
+    public partial class MainContent : UserControl
+    {
+        public MainContent()
+        {
+            InitializeComponent();
+        }
+        private bool obtenerFoliosYLinks()
+        {
+            bool result = false;
+            try
+            {
+                //urls = new Dictionary<string, string>();
+                //document = webbrowser.Document;
+                //links = document.GetElementsByTagName("span");
+                //folios = document.GetElementsByTagName("input");
+                //links = links.GetElementsByName("BtnDescarga");
+                //folios = folios.GetElementsByName("ListaFolios");
+                result = true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+
+        }
+
+        private void validaDescarga()
+        {
+            try
+            {
+                if (webBrowser.Source.ToString().StartsWith("https://portalcfdi.facturaelectronica.sat.gob.mx"))
+                {
+                    if (obtenerFoliosYLinks())
+                    {
+                        //realizaDescarga();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Primero filtre los folios a descargar");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        // Events
+        private void mnDownload_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("mnDownload_Click");
+        }
+
+        private void mnGoTo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("mnGoTo_Click");
+            validaDescarga();
+
+
+        }
+
+        private void mnSettings_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("mnSettings_Click");
+
+        }
+
+        private void mnExport_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("mnExport_Click");
+
+        }
+    }
+}
---
+++ brainstorm_contable/UserControls/MenuItem.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace brainstorm_contable.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MenuItem.xaml
+    /// </summary>
+    public partial class MenuItem : UserControl
+    {
+        public ImageSource IconSource
+        {
+            get { return (ImageSource)GetValue(IconSourceProperty); }
+            set { SetValue(IconSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconSourceProperty =
+            DependencyProperty.Register(
+                "IconSource",
+                typeof(ImageSource),
+                typeof(MenuItem),
+                new UIPropertyMetadata());
+        public string TextContent
+        {
+            get { return (string)GetValue(TextContentProperty); }
+            set { SetValue(TextContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextContentProperty =
+            DependencyProperty.Register(
+                "TextContent",
+                typeof(string),
+                typeof(MenuItem),
+                new UIPropertyMetadata());
+
+        
+        public event EventHandler Click;
+
+        public MenuItem()
+        {
+            InitializeComponent();
+            btn.Click += HandleClick;
+        }
+
+        private void HandleClick(object sender, RoutedEventArgs e)
+        {
+            OnClick(EventArgs.Empty);
+        }
+
+        protected virtual void OnClick(EventArgs e)
+        {
+            Click?.Invoke(this, e);
+        }
+    }
+}
